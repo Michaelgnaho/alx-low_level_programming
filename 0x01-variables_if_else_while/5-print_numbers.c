310b9402fe1857bb970822bce24da48520dff61f
Michael Gnaho <gnahomichael@gmail.com>
2023-10-06 09:42:10 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this fuction prints from zero to nine
+ *
+ * Return: 0 when it is a success
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+	printf("\n");
+
+	return (0);
+}
