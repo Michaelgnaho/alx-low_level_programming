86be0d082ceb07e284b6e614fc091ed033675391
Michael Gnaho <gnahomichael@gmail.com>
2023-10-06 14:59:00 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - this code is a nested loop
+ *
+ * Return: 0 when it is a success
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
