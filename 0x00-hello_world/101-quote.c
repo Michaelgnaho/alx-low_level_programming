d452a74737cefff3788c35574ab3d8cd3fc04a22
Michael Gnaho <gnahomichael@gmail.com>
2023-10-07 07:39:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this fubctions prints in unix format
+ *
+ * Return: 0 when it is a success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
