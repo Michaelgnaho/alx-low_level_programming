757f0d90385ae0a0a235d985fd3db18b11b72590
Michael Gnaho <gnahomichael@gmail.com>
2023-10-10 12:47:51 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function prints complete alphabet 10 times
+ *
+ * Return: 0 when it is a success;
+ */
+
+void print_alphabet_x10(void)
+{
+	int m;
+	char j;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+
+	}
+	_putchar('\n');
+}
+
