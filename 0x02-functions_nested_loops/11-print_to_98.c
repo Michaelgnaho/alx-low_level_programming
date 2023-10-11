f73fd5a0059cf5e05e93e48904792a6823d65bb7
Michael Gnaho <gnahomichael@gmail.com>
2023-10-11 11:19:17 -0700

    natural_number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 	int g;
 
-	if (n == 0 && n < 98)
+	if (n >= 0 && n < 98)
 	{
 		for (g = n; g <= 98; g++)
 		{
@@ -30,7 +30,7 @@ void print_to_98(int n)
 		{
 			printf("%d", g);
 			if (g != 98)
-				printf(", ");
+			printf(", ");
 		}
 		printf("\n");
 	}
