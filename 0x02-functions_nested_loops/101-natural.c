c9a27b2515315c89d6b69842bda4cbc589485c74
Michael Gnaho <gnahomichael@gmail.com>
2023-10-11 14:23:40 -0700

    multiples of 5 and 3 below 1040

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this fuction prints the multiples of 3 and 5
+ *
+ * Return: 0 if it is a success
+ */
+
+int main(void)
+{
+	int m;
+	int sum = 0;
+
+	for (m = 0; m < 1042; m++)
+	{
+		if (m % 3 == 0 || m % 5 == 0)
+			sum = sum + m;
+	}
+	printf("%d", sum);
+}
