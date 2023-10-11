21c11802e37b14044866eab5d80e6de1959ce84e
Michael Gnaho <gnahomichael@gmail.com>
2023-10-11 09:54:49 -0700

    add_int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - this function adds two ints
+ *
+ * @m: first parameter
+ *
+ * @g: second parameter
+ *
+ * Return: m + g
+ */
+
+int add(int m, int g)
+{
+
+	return (m + g);
+}
