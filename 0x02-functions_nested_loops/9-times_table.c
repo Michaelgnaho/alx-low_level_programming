e1a0a7f97f3f1c36fa78ac2021d390bc0e2b27d4
Michael Gnaho <gnahomichael@gmail.com>
2023-10-11 09:24:33 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - this function prints times table
+ *
+ * Return: 0 when it is a success
+ */
+
+void times_table(void)
+{
+	int m;
+	int g;
+	int result;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (g = 0; g < 10; g++)
+		{
+			result = g * m;
+			if (g == 0)
+				printf("%d", result);
+			else
+			{
+				printf("%2d", result);
+				if (g != 9)
+					printf(", ");
+			}
+
+		}
+		printf("\n");
+	}
+
+}
