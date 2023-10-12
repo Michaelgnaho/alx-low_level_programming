758d5707fde1499d27c6e2f2f13a4effe90e34cc
Michael Gnaho <gnahomichael@gmail.com>
2023-10-12 12:57:06 -0700

    square #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square- this number print square
+ *
+ * @size: parameter
+ * Return: 0 when it is a sucess
+ */
+
+void print_square(int size)
+{
+	if (size == 0)
+		_putchar('\n');
+	else
+	{
+		int m;
+		int g;
+
+		for (m = 0; m < size; m++)
+		{
+			for (g = 0; g < size; g++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
