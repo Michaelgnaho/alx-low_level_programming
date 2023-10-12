fb09c72e6afb0e43786156fc27aedcf1e6834d0d
Michael Gnaho <gnahomichael@gmail.com>
2023-10-12 09:35:22 -0700

    mutiply two numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * mul- this function print collaboration of multiplication
+ * mul - this function print collaboration of multiplication
  * @a: this is the first parameter
  * @b: this is the second parameter
- * Retuen: 0 when it is a success
+ * Return: 0 when it is a success
  */
 
 int mul(int a, int b)
