408cf501078b1f63f17e3d78c388af97faaff988
Michael Gnaho <gnahomichael@gmail.com>
2023-10-12 13:27:26 -0700

    fiz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- this code prints fizz buzz
+ *
+ * Return: 0 when it is a sucess
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 100; m++)
+	{
+		if ((m % 3 == 0) && (m % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (m % 3 == 0)
+			printf("%s", "Fizz");
+		else if (m % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", m);
+	}
+	printf("\n");
+
+}
+
