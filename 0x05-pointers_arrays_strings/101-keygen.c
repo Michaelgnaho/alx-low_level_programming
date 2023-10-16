067ebb8762242bc0d89ab06d553651c181414975
Michael Gnaho <gnahomichael@gmail.com>
2023-10-16 14:46:38 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this func generate password
+ *
+ * Return: 0 when it is a success
+ */
+int main(void)
+{
+	int pass[100];
+	int m, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (m = 0; m < 100; m++)
+	{
+		pass[m] = rand() % 78;
+		sum += (pass[m] + '0');
+		putchar(pass[m] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
