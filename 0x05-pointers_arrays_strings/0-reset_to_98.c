e180275620b222b55c8cec02809a951302693de3
Michael Gnaho <gnahomichael@gmail.com>
2023-10-16 10:08:23 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98- this function print 98 with pointer
+ * @n: the is a pointer parameter
+ *
+ * Return: 0 when it is a sucess
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
