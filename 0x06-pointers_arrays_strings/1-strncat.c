2d214d8a51e4ec9ef685960465699d3d3785528c
Michael Gnaho <gnahomichael@gmail.com>
2023-10-18 10:21:11 -0700

    string concatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat- this func print a string in reverze
+ * @dest: first string
+ * @src: second string
+ * @n: the number
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lent = strlen(dest);
+	int m;
+
+	for (m = 0; m < n && m != '\0'; m++)
+	{
+		dest[lent + m] = src[m];
+		src++;
+	}
+	dest[lent + m] = '\0';
+	return (dest);
+
+
+}
