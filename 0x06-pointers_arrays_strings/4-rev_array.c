f69b358e349e8c79a8226c595ad4167e508254a3
Michael Gnaho <gnahomichael@gmail.com>
2023-10-19 09:44:19 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- this func print a string in reverze
+ * @a: first integer
+ * @n: second integer
+ *
+ * Return: 0 when it is a success
+ */
+
+void reverse_array(int *a, int n)
+{
+	int m,temp;
+
+	for (m = 0; m < n / 2; m++)
+	{
+		temp = a[m];
+		a[m] = a[n - 1 - m];
+		a[n - 1 - m] = temp;
+	}
+
+
+
+}
