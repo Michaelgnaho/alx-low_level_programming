0b07036de74c9907aaf7cec0daf52b0dd4c5ff2b
Michael Gnaho <gnahomichael@gmail.com>
2023-10-23 07:01:08 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr- this func locate characters in a string
+ * @s: the string to be checked
+ * @c: the character to locate
+ * Return: 0 when it is a success
+ */
+
+char *_strchr(char *s, char c)
+{
+	int m;
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		if (s[m] == c)
+			return (s + m);
+	}
+	return (NULL);
+
+}
