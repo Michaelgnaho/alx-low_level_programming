070b00877329114c922ef3d129c3205f288899c5
Michael Gnaho <gnahomichael@gmail.com>
2023-10-23 09:01:58 -0700

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk- this func search a string for a set for a set of byte
+ * @s: where to check
+ * @accept: what to check
+ * Return: 0 when it is a success
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		const char *m = accept;
+
+		while (*m != '\0')
+		{
+			if (*s == *m)
+				return (s);
+			m++;
+		}
+		s++;
+	}
+	return (NULL);
+
+
+}
