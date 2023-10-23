0ad7d87dcb7905b7fb43490e82131c3efe4b423f
Michael Gnaho <gnahomichael@gmail.com>
2023-10-23 10:03:09 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - this function set the alue of a pointer to char
+  *
+  * @s: where to set
+  *
+  * @to: what to set
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
