988059e8aba2f1bfb13fc2d7769093938c97212c
Michael Gnaho <gnahomichael@gmail.com>
2023-10-25 08:36:29 -0700

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * _pow_recursion - this function print the value of  x raised to y
+  * @x: parameter 1
+  * @y: parameter 2
+  * Return: value of a given number passed to the function
+  */
+
+
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+	return (-1);
+
+else if (y == 0)
+	return (1);
+
+else
+	return (x * _pow_recursion(x, (y - 1)));
+
+return (0);
+}
+
