1fdedfe47ea7d2d025049138ba416ce55f1652d8
Michael Gnaho <gnahomichael@gmail.com>
2023-10-28 07:20:39 -0700

    new file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ * Return: 0 when it is a success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
