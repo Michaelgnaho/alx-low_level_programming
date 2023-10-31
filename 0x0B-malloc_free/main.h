6a81fb8b9ffc0a027f6b1bde361986d5f1e1f15b
Michael Gnaho <gnahomichael@gmail.com>
2023-10-31 13:19:07 -0700

    100-argstostr.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
