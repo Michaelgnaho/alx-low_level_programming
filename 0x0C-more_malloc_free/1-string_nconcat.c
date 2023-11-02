dc3f9a120177ef7f8e2c7558d22e20479b3dce27
Michael Gnaho <gnahomichael@gmail.com>
2023-11-02 19:52:28 +0100

    1-string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat- this fuction concatenate memory using malloc
+ * @s1: the first parameter
+ * @s2: the second parameter
+ * @n: number to be copied from s2
+ * Return: 0 if it is a success
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int m;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (m = 0; s1[m] != '\0'; m++)
+		s1len++;
+	for (m = 0; s1[m] != '\0'; m++)
+		s2len++;
+
+	result = malloc(sizeof(s1len + n) + 1);
+
+		if (result == NULL)
+			return (NULL);
+		if (n >= s2len)
+		{
+			for (m = 0; s1[m] != '\0'; m++)
+				result[m] = s1[m];
+			for (m = 0; s2[m] != '\0'; m++)
+				result[s1len + m] = s2[m];
+			result[s1len + m] = '\0';
+		}
+		else
+		{
+			for (m = 0; s1[m] != '\0'; m++)
+				result[m] = s1[m];
+			for (m = 0;  m < n; m++)
+				result[s1len + m] = s2[m];
+
+			result[s1len + m] = '\0';
+		}
+		return (result);
+
+}
